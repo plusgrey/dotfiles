e423e64e3fac2d464eff622735cd9a11613d38e0
plusgrey <plusgrey@yahoo.com>
2026-02-05 20:03:54 +0800

    Add ghostty config and remove outdated nvim config

--- .config/nvim.bak/init.lua
+++
@@ -1,8 +0,0 @@
--- This is the main configuration file for Neovim
--- load config settings
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-
--- load lazy plugins
-require("config.lazy")
