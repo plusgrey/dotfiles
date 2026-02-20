466e717da2cab553d2e5ff34b7207828d9a5a1a0
jhwang <jhwang@bytedance.com>
2026-02-20 15:23:09 +0800

    Update gitignore

--- .config/emacs/theme/theme.el
+++ .config/emacs/theme/theme.el
@@ -1,17 +1,6 @@
-;;; theme.el --- Light Eye-Care Theme Configuration -*- lexical-binding: t -*-
-
-(use-package doom-themes
+(use-package vscode-dark-plus-theme
   :ensure t
   :config
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t)
-  (load-theme 'doom-zenburn t)
-
-  ;; 在使用选定主题时，让光标处的括号高亮更明显
-  (doom-themes-visual-bell-config)
-  
-  ;; 修正某些 UI 元素的显示 (可选)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config))
+  (load-theme 'vscode-dark-plus t))
 
 (provide 'theme)
